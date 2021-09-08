ec714c0e441bd4ff69cca172838cc62999f5c52f
Ashwin20102000 <jkashwin2@gmail.com>
2021-09-08 17:38:33 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -11,12 +11,33 @@ export default function App() {
         <button onClick={() => setPagination(1)} className="btn btn-dark">
           Salary Structure
         </button>
-        <button
+        {/* <button
           onClick={() => setPagination(2)}
           className="btn btn-dark border mx-3"
         >
           Payslips
+        </button> */}
+        <button
+          type="button"
+          class="btn btn-primary"
+          data-toggle="modal"
+          data-target=".bd-example-modal-lg"
+        >
+          Payslip
         </button>
+
+        <div
+          class="modal fade bd-example-modal-lg"
+          tabindex="-1"
+          role="dialog"
+          aria-labelledby="myLargeModalLabel"
+          aria-hidden="true"
+        >
+          <div class="modal-dialog modal-lg">
+            <div class="modal-content">...</div>
+          </div>
+        </div>
+
         <button
           onClick={() => setPagination(3)}
           className="btn btn-dark border"
